e652d894a6f86a8456e30998eedfb35176bd26ab
Frank Geerlings <frank@frankgeerlings.com>
2015-03-17 23:57:47 +0100

    Initial commit

---
+++ getstats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import json, httplib
+from pprint import pprint
+
+conn = httplib.HTTPConnection('stats.grok.se')
+conn.request("GET", "/json/nl/latest90/Geschiedenis_van_de_elektriciteit")
+
+r = conn.getresponse()
+print r.status, r.reason
+
+body = r.read()
+
+data = json.loads(body)
+
+d = data['daily_views']
+
+from operator import itemgetter
+
+lis = [(x, d[x]) for x in d]
+biggest = max(lis,key=itemgetter(1))
+lis.remove(biggest)
+
+from numpy import std, mean
+figures = [x[1] for x in lis]
+
+m = mean(figures)
+print 'Biggest:', biggest
+print 'Mean: ', m
+print 'Std:  ', std(figures)
+print 'Popularity increase factor: ', biggest[1] / m
