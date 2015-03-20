01c282e93450f7a3421986cd13cc19c3adb266ec
Frank Geerlings <frank@frankgeerlings.com>
2015-03-20 23:40:04 +0100

    As a function

--- getstats.py
+++ getstats.py
@@ -3,32 +3,34 @@
 import json, httplib
 from pprint import pprint
 
-conn = httplib.HTTPConnection('stats.grok.se')
-conn.request("GET", "/json/nl/latest90/Geschiedenis_van_de_elektriciteit")
+def stats(article):
+	conn = httplib.HTTPConnection('stats.grok.se')
+	conn.request("GET", "/json/nl/latest90/%s" % article)
 
-r = conn.getresponse()
-print r.status, r.reason
+	r = conn.getresponse()
 
-body = r.read()
+	body = r.read()
 
-data = json.loads(body)
+	data = json.loads(body)
 
-d = data['daily_views']
+	d = data['daily_views']
 
-from operator import itemgetter
+	from operator import itemgetter
 
-lis = [(x, d[x]) for x in d]
-biggest = max(lis,key=itemgetter(1))
-lis.remove(biggest)
+	lis = [(x, d[x]) for x in d]
+	biggest = max(lis,key=itemgetter(1))
+	lis.remove(biggest)
 
-from numpy import std, mean
-figures = [x[1] for x in lis]
+	from numpy import std, mean
+	figures = [x[1] for x in lis]
 
-m = mean(figures)
-total = sum(figures) + biggest[1]
-print 'Biggest:', biggest
-print 'Mean: ', m
-print 'Std:  ', std(figures)
-print 'Popularity increase factor: ', biggest[1] / m
-print 'Number of standard deviations from the mean: ', (biggest[1] - m) / std(figures)
-print 'Total visits over last 90 days: ', total
+	m = mean(figures)
+	total = sum(figures) + biggest[1]
+	print 'Biggest:', biggest
+	print 'Mean: ', m
+	print 'Std:  ', std(figures)
+	print 'Popularity increase factor: ', biggest[1] / m
+	print 'Number of standard deviations from the mean: ', (biggest[1] - m) / std(figures)
+	print 'Total visits over last 90 days: ', total
+
+stats('Vanguard_1')
